f0de7aa19c7a2244ebb1d7c34383602e5887f552
francodavyd <sanchezfranco7d@gmail.com>
2024-11-26 11:39:56 -0300

    Configurando tests

---
+++ productos-sv/src/test/java/com/francodavyd/repository/IProductoRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.francodavyd.repository;
+
+import com.francodavyd.model.Producto;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class IProductoRepositoryTest {
+    @Autowired
+    private IProductoRepository repository;
+    private Producto producto;
+    @BeforeEach
+    public void setUp(){
+        producto = new Producto();
+        producto.setId(1L);
+        producto.setNombre("Aceite");
+        producto.setDescripcion("Aceite de girasol 1L");
+        producto.setPrecio(new BigDecimal(1600));
+        producto.setStockDisponible(10);
+        producto.setStockReservado(0);
+        producto.setCategoria("Comida");
+    }
+    @DisplayName("Test para guardar un producto")
+    @Test
+    public void saveTest(){
+        Producto prod = repository.save(producto);
+        assertThat(prod).isNotNull();
+        assertThat(prod.getId()).isNotNull();
+        assertThat(prod.getNombre()).isEqualTo("Aceite");
+    }
+    @DisplayName("Test para obtener lista de productos")
+    @Test
+    public void getAllTest(){
+        repository.save(producto);
+        List<Producto> list = repository.findAll();
+        assertThat(list).isNotNull();
+        assertThat(list.size()).isEqualTo(1);
+    }
+    @DisplayName("Test para obtener producto por ID")
+    @Test
+    public void findByIdTest(){
+        repository.save(producto);
+        Producto prod = repository.findById(producto.getId()).get();
+        assertThat(prod).isNotNull();
+        assertThat(prod.getNombre()).isEqualTo("Aceite");
+    }
+    @DisplayName("Test para eliminar un producto")
+    @Test
+    public void deleteByIdTest(){
+        repository.save(producto);
+        repository.deleteById(producto.getId());
+        Optional<Producto> findAuthor = repository.findById(producto.getId());
+        assertThat(findAuthor).isEmpty();
+    }
+    @DisplayName("Test para actualizar un producto")
+    @Test
+    public void updateTest(){
+        repository.save(producto);
+
+        Producto prod = repository.findById(producto.getId()).get();
+        prod.setNombre("Carne");
+        prod.setCategoria("Alimentos");
+        Producto prodAct = repository.save(prod);
+
+        assertThat(prodAct).isNotNull();
+        assertThat(prodAct.getId()).isEqualTo(producto.getId());
+        assertThat(prodAct.getNombre()).isEqualTo("Carne");
+        assertThat(prodAct.getCategoria()).isEqualTo("Alimentos");
+    }
+}
